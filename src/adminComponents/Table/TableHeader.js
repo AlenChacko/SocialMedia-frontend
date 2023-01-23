6f04a0f9fa5e6eb85f2f3cfd999f6e061a331ba5
Alen Chacko <alenchacko92@gmail.com>
2023-01-23 13:40:07 +0530

    admin components edited

--- src/adminComponents/Table/TableHeader.js
+++ src/adminComponents/Table/TableHeader.js
@@ -2,18 +2,23 @@ import { TableCell, TableHead, TableRow } from "@mui/material";
 import React from "react";
 
 const TableHeader = (props) => {
-
   return (
     <>
-      <TableHead sx={{ backgroundColor: "aquamarine", color: "white", borderBottom:'2px solid grey' }}>
-            <TableRow>
-            {props?.data?.map((obj) => {
-          return (
+      <TableHead
+        sx={{
+          backgroundColor: "aquamarine",
+          color: "white",
+          borderBottom: "2px solid grey",
+        }}
+      >
+        <TableRow>
+          {props?.data?.map((obj) => {
+            return (
               <TableCell align="center">
                 <b>{obj}</b>
               </TableCell>
-          );
-        })}
+            );
+          })}
         </TableRow>
       </TableHead>
     </>
